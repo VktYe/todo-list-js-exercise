f4ec4f426205cc96f210b69ef8b9fa1f36a9a8e3
VktYe <v.yehorenko@gmail.com>
2025-03-12 21:13:28 -0300

    removed unnecessary comments

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ function newTask(title, description) {
   };
   return task;
 }
-// // Arrays to keep track of each task's state
-// const taskTitles = [];
-// const taskComplete = [];
-// const taskDescription = [];
-
-// // Create a new task by adding to the arrays
-// // A new task will be created as incomplete
-// function newTask(title, description) {
-//   taskTitles.push(title);
-//   taskDescription.push(description);
-//   taskComplete.push(false);
-// }
-
-
-// Print the state of a task to the console in a nice readable way
-// function logTaskState(task) {
-//     console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-// }
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-// function completeTask(task) {
-//   task.complete = true;
-// }
-
 
 // DRIVER CODE BELOW
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
